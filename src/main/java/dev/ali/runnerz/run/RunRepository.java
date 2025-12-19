df94bdbad92fb8aee70ef5929749bb496254870d
alibu <alibu@Z590>
2025-12-19 16:41:42 +0300

    This is also more commit

--- src/main/java/dev/ali/runnerz/run/RunRepository.java
+++ src/main/java/dev/ali/runnerz/run/RunRepository.java
@@ -3,6 +3,7 @@ package dev.ali.runnerz.run;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Repository;
 
@@ -19,10 +20,26 @@ public class RunRepository {
 	}
 	
 	
-	Run findById(Integer id) {
-		return runs.stream().filter(run->run.id() == id).findFirst().get();
+	Optional<Run> findById(Integer id) {
+		return runs.stream().filter(run->run.id() == id).findFirst();
 	}
 	
+	
+	void create(Run run) {
+	runs.add(run);
+	}
+	
+	void update(Run run,Integer id) {
+		Optional<Run> existingRun = findById(id);
+		if(existingRun.isPresent()) {
+			runs.set(runs.indexOf(existingRun), run);
+		}
+	}
+	
+	
+	void delete(Integer id) {
+		runs.removeIf(run -> run.id().equals(id));
+	}
 	
 	@PostConstruct
 	private void init() {
