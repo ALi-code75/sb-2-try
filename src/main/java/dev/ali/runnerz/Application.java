17e30c8318992b337181ae869517411d108bbe01
alibu <alibu@Z590>
2025-12-19 15:32:34 +0300

    this is it

--- src/main/java/dev/ali/runnerz/Application.java
+++ src/main/java/dev/ali/runnerz/Application.java
@@ -1,10 +1,16 @@
 package dev.ali.runnerz;
 
+import java.time.LocalDateTime;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import dev.ali.runnerz.run.Location;
+import dev.ali.runnerz.run.Run;
 
 
 
@@ -18,9 +24,27 @@ public class Application {
 		
 		SpringApplication.run(Application.class, args);
 		
-		log.info("Application has started successfully");
 		
 		
+		
+	}
+	
+	
+	
+	@Bean
+	CommandLineRunner runner() {
+	    return args -> {
+	        Run run = new Run(
+	            1,
+	            "First Run",
+	            LocalDateTime.of(2021, 1, 1, 6, 0),
+	            LocalDateTime.of(2021, 1, 1, 7, 0),
+	            5,
+	            Location.OUTDOOR
+	        );
+
+	        System.out.println(run);
+	    };
 	}
 
 }
