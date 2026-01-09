2ccd8a96fa7bc5752bdbd321f72bf2e905da20f0
alibu <alibu@Z590>
2026-01-09 16:48:39 +0300

    docker change

--- src/main/java/dev/ali/runnerz/Application.java
+++ src/main/java/dev/ali/runnerz/Application.java
@@ -1,36 +1,18 @@
+
 package dev.ali.runnerz;
 
-import java.time.LocalDateTime;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import dev.ali.runnerz.run.Location;
-import dev.ali.runnerz.run.Run;
-import dev.ali.runnerz.run.RunRepository;
 
 
 
 @SpringBootApplication
 public class Application {
 
-	private static final Logger log = LoggerFactory.getLogger(Application.class);
 	public static void main(String[] args) {
-		
-		
-		
 		SpringApplication.run(Application.class, args);
-		
-		
-		
-		
-	}
-	
-	
+	}	
 	
 //	@Bean
 //	CommandLineRunner runner(RunRepository runRepository) {
